b9c461ea746a8bc5b34e3616f6dc9ddad38aaa80
tom.whitehead <tom.whitehead@9fin.com>
2025-08-28 09:12:00 +0100

    git blame plugin

--- init.lua
+++ init.lua
@@ -1007,6 +1007,13 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  {
+    'FabijanZulj/blame.nvim',
+    lazy = false,
+    config = function()
+      require('blame').setup {}
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
